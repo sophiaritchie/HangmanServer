750ddfe63338f76b3f77ef2d0014a142f6cf0b72
Sophia Ritchie <sophritchie@gmail.comgit config --global user.name Sophia Ritchiegit config --global user.email sophritchie@gmail.com>
2020-04-29 16:13:24 +1200

    Schema, Dummy POST /games and test

--- index.js
+++ index.js
@@ -1,9 +1,58 @@
 var express = require("express");
 var app = express();
+const Joi = require('joi');
+var request = require("request");
+var fs = require("fs");
+
 app.use(express.json());
 
- app.get('/ping', (req, res) => {
-     res.send('ping');
-     });
+app.get('/ping', (req, res) => {
+    res.send('ping');
+});
+
+var gamesDb = [
+]
+
+const schema = {
+    id: Joi.number().required(),
+    word: Joi.string().min(3).required(),
+    wordLength: Joi.number().required(),
+    guesses: Joi.array().required(),
+    lives: Joi.number().required(),
+    status: Joi.string().valid('active', 'won', 'lost')
+};
+
+app.post('/api/games', (req, res) => {
+
+    wordArray = [];
+    // request('https://raw.githubusercontent.com/dwyl/english-words/master/words_alpha.txt', { raw: true }, (err, res, body) => {
+    //     if (err) { return console.log(err); }
+    //     //console.log(res);
+    // });
+
+    var randomWord = "hello";
+    const generatedId = 1
+
+    const game = {
+        id: generatedId,
+        word: randomWord,
+        wordLength: randomWord.length,
+        guesses: [],
+        lives: 3,
+        status: 'active'
+    };
+
+    const { error } = Joi.validate(game, schema);
+    if (error) {
+        res.status(400).send(error.details[0].message)
+        return;
+    }
+
+    gamesDb.push(game);
+    res.send(game);
+});
+
 
 app.listen(3000);
+
+module.exports = app;
--- test/games.js
+++ test/games.js
@@ -1,17 +1,40 @@
 var expect = require("chai").expect;
 let chai = require('chai');
 let chaiHttp = require('chai-http');
+var server = require('../index')
 chai.use(chaiHttp);
 
 describe("Games API", function () {
     describe("GET /ping", function () {
         it("Returns 200, ping", function (done) {
             chai.request('http://localhost:3000')
-            .get('/ping')
-            .end((err, res) => {
-                expect(res.statusCode).to.equal(200);
-                done();
-            });
+                .get('/ping')
+                .end((err, res, body) => {
+                    expect(res.statusCode).to.equal(200);
+                    done();
+                });
+        });
+    });
+
+    describe("POST /games", function () {
+        var url = "http://localhost:3000/api/games";
+
+        it("Returns 200", function (done) {
+            chai.request('http://localhost:3000')
+                .post('/api/games')
+                .end((err, res, body) => {
+                    expect(res.statusCode).to.equal(200);
+                    console.log(res.body)
+                    expect(res.body).to.contain(
+                        {
+                            wordLength: 5,
+                            lives: 3,
+                            guesses: [],
+                            status: 'active'
+                        }
+                    )
+                    done();
+                });
         });
     });
 });
